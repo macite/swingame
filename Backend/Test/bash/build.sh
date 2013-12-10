853246f6a74a67783b0c957bc0ab2db296c8f656
James Armstrong <itsjarm@gmail.com>
2013-12-10 11:24:35 +0000

    Added build.sh for test files

---
+++ Backend/Test/bash/build.sh
@@ -0,0 +1,4 @@
+clang++ -std=c++11 ../src/*.cpp -I../../SGSDL2/src/ -I../../Core/src/ -I../../Core/include/ -I/usr/include/SDL2 -lSDL2 -lSDL2_mixer -lsgsdl2
+mv a.out ../Resources/a.out
+cd ../Resources
+./a.out
