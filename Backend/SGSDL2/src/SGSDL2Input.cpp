6e5fc73f33da46027342cf1c95d400e30f499718
hellola <ewoudt.kellerman@gmail.com>
2013-12-11 16:49:54 +1100

    fix wrong function name, add  libttf to build script

--- Backend/SGSDL2/src/SGSDL2Input.cpp
+++ Backend/SGSDL2/src/SGSDL2Input.cpp
@@ -213,6 +213,6 @@ void sgsdl2_load_input_fns(sg_interface *functions)
     functions->input.mouse_state = &SDL_GetMouseState;  // call it directly
     functions->input.mouse_relative_state = &SDL_GetRelativeMouseState;
     functions->input.mouse_cursor_state = &SDL_ShowCursor; // 0 hide, 1 show, -1 query
-    functions->input.start_unicode_text_input = &sgsdl2_unicode_start_text_input; 
+    functions->input.start_unicode_text_input = &sgsdl2_start_unicode_text_input; 
     functions->input.stop_unicode_text_input = &SDL_StopTextInput; 
 }
--- Backend/Test/bash/build.sh
+++ Backend/Test/bash/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm ../Resources/a.out
-clang++ -std=c++11 ../src/*.cpp -I../../SGSDL2/src/ -I../../Core/src/ -I../../Core/include/ -I/usr/include/SDL2 -lSDL2 -lSDL2_mixer -lsgsdl2
+clang++ -std=c++11 ../src/*.cpp -I../../SGSDL2/src/ -I../../Core/src/ -I../../Core/include/ -I/usr/include/SDL2 -lSDL2 -lSDL2_mixer -lSDL2_ttf -lsgsdl2
 mv a.out ../Resources/a.out
 cd ../Resources
 ./a.out
