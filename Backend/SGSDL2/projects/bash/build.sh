640e655fe7dc82a454a1a28f815316dff2e6955c
James Armstrong <itsjarm@gmail.com>
2013-12-12 16:39:16 +1100

    Enable warnings on library build as terribad SDL warnings are gone.

--- Backend/SGSDL2/projects/bash/build.sh
+++ Backend/SGSDL2/projects/bash/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm libsgsdl2.so
-clang++ -shared -fPIC -std=c++11 -Wno-padded -Wno-missing-prototypes -Wno-c++98-compat -lSDL2 -lSDL2_mixer -lSDL2_image -lSDL2_gfx -I../../../Core/include/ -I../../../Core/src/ ../../../SGSDL2/src/*.cpp ../../../Core/src/*.cpp -o libsgsdl2.so -g
+clang++ -shared -fPIC -std=c++11 -Weverything -Wno-padded -Wno-missing-prototypes -Wno-c++98-compat -lSDL2 -lSDL2_mixer -lSDL2_image -lSDL2_gfx -I../../../Core/include/ -I../../../Core/src/ ../../../SGSDL2/src/*.cpp ../../../Core/src/*.cpp -o libsgsdl2.so -g
 sudo install libsgsdl2.so -t /usr/lib/
