b51175322633323b8f4ccd2936c683e5c88ad852
hellola <ewoudt.kellerman@gmail.com>
2014-06-23 10:03:58 +1000

    produce RunTests instead of a.out and produce it in the current directory

--- Backend/Test/bash/build.sh
+++ Backend/Test/bash/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 rm ../Resources/a.out
-clang++ -std=c++11 ../src/*.cpp -I../../SGSDL2/src/ -I../../Core/src/ -I../../Core/include/ -I/usr/include/SDL2 -lSDL2 -lSDL2_mixer -lSDL2_ttf -lsgsdl2 -g
-mv a.out ../Resources/a.out
-cd ../Resources
-./a.out
+clang++ -std=c++11 ../src/*.cpp -I../../SGSDL2/src/ -I../../Core/src/ -I../../Core/include/ -I/usr/include/SDL2 -lSDL2 -lSDL2_mixer -lSDL2_ttf -lsgsdl2 -g -o RunTests
+./RunTests
