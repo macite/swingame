82e12d1add0c81e4f3ccc40c8a443689c52e8026
Andrew Cain <acain@swin.edu.au>
2016-01-26 21:38:27 -0800

    FIX: get latest version working on linux

--- Backend/SGSDL2/projects/bash/build.sh
+++ Backend/SGSDL2/projects/bash/build.sh
@@ -4,7 +4,7 @@
 #sudo apt-get install build-essential libsdl2-dev libsdl2-gfx-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-net-dev libsdl2-ttf-dev
 
 rm libsgsdl2.so
-clang++ -shared -fPIC -std=c++11 -Weverything -Wno-padded -Wno-missing-prototypes -Wno-c++98-compat -lSDL2 -lSDL2_mixer -lSDL2_image -lSDL2_gfx -lSDL2_ttf -lSDL2_net -I../../../Core/include/ -I../../../Core/src/ ../../../SGSDL2/src/*.cpp ../../../Core/src/*.cpp -o libsgsdl2.so -g
+clang++ -shared -fPIC -std=c++11 -Weverything -Wno-padded -Wno-missing-prototypes -Wno-c++98-compat -lSDL2 -lSDL2_mixer -lSDL2_image -lSDL2_gfx -lSDL2_ttf -lSDL2_net -lcurl -I../../../Core/include/ -I../../../Core/src/ ../../../SGSDL2/src/*.cpp ../../../Core/src/*.cpp -o libsgsdl2.so -g
 
 sudo install libsgsdl2.so -t /usr/lib/
 
